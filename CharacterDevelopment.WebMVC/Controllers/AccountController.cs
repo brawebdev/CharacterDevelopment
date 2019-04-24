b761044a18f7d90de0a35c8a1f4f6ac2c66f6ba6
Ben Arnold <BenA@knowledgeservices.com>
2019-04-24 09:05:12 -0400

    Require HTTPS when not in Debug

--- CharacterDevelopment.WebMVC/Controllers/AccountController.cs
+++ CharacterDevelopment.WebMVC/Controllers/AccountController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +10,10 @@ using CharacterDevelopment.Data;
 
 namespace CharacterDevelopment.WebMVC.Controllers
 {
+#if !DEBUG
+    [RequireHttps]
+#endif
+
     [Authorize]
     public class AccountController : Controller
     {
@@ -424,7 +425,7 @@ namespace CharacterDevelopment.WebMVC.Controllers
             base.Dispose(disposing);
         }
 
-        #region Helpers
+#region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
 
@@ -481,6 +482,6 @@ namespace CharacterDevelopment.WebMVC.Controllers
                 context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
         }
-        #endregion
+#endregion
     }
 }
