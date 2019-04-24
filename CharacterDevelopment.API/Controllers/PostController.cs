2097ced7ed45c4bcc09556526b953d2e13ab4a08
Ben Arnold <BenA@knowledgeservices.com>
2019-04-24 14:05:32 -0400

    Post API Controller with Get

---
+++ CharacterDevelopment.API/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Http;
+using CharacterDevelopment.Services;
+using Microsoft.AspNet.Identity;
+
+namespace CharacterDevelopment.API.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+    }
+}
